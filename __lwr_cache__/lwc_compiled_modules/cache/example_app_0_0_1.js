528c2736160f76cd6653c4f3b8b0957e35e4587c
Alan McClenaghan <alandmcclenaghan@icloud.com>
2026-03-25 19:19:34 +0000

    Set up and first commit

---
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_0_0_1.js
@@ -0,0 +1,8 @@
+import _tmpl from "./app.html";
+import { registerComponent as _registerComponent, LightningElement } from "lwc";
+class HelloWorldApp extends LightningElement {
+  /*LWC compiler v2.38.1*/
+}
+export default _registerComponent(HelloWorldApp, {
+  tmpl: _tmpl
+});
