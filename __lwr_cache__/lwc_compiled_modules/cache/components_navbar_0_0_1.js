c8bc8d74fed1ee851245703c6401b13bc4d7e615
Alan McClenaghan <alandmcclenaghan@icloud.com>
2026-03-26 19:41:10 +0000

    navbar

---
+++ __lwr_cache__/lwc_compiled_modules/cache/components_navbar_0_0_1.js
@@ -0,0 +1,8 @@
+import _tmpl from "./navbar.html";
+import { registerComponent as _registerComponent, LightningElement } from "lwc";
+class Navbar extends LightningElement {
+  /*LWC compiler v2.38.1*/
+}
+export default _registerComponent(Navbar, {
+  tmpl: _tmpl
+});
